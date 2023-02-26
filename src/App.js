31eb49623183d1f51a987cf5c9b12b599f8ee787
IshSonP5 <119278973+IshSonP5@users.noreply.github.com>
2023-02-26 00:20:59 -0800

    like

--- src/App.js
+++ src/App.js
@@ -9,11 +9,13 @@ function App() {
     {
       name: "Host 1",
       message: "I am ready to go!",
+      liked: false,
       id: 1
     },
     {
       name: "Host 2",
       message: "I am not ready to go!",
+      liked: false,
       id: 2
     }
   ]);
@@ -34,6 +36,19 @@ function App() {
     });
   }
 
+  function toggleLike(id) {
+    setMessages(oldMessages => {
+      // Step 1: clone the old value to separate the old from the new.
+      const newMessages = structuredClone(oldMessages);
+      // Step 2: update the new value
+      const toggledItem = newMessages.find(message => message.id === id);
+      toggledItem.liked = !toggledItem.liked;
+
+      // Step 3: return the updated new value
+      return newMessages;
+    });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -41,7 +56,7 @@ function App() {
       </header>
       <main>
         <TopicForm addItem={addItem} />
-        <MessageList messages={messages} />
+        <MessageList messages={messages} toggleLike={toggleLike} />
       </main>
       <Footer />
     </div>
